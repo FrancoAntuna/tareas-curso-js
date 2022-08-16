cbad00014dea30659a25c7b489b069b211890600
FrancoAntuna <franco.antuna.nob@gmail.com>
2022-08-15 21:05:11 -0300

    se corrijen las busquedas del documento
    se reemplazo la propiedad .innerText por .value, los input se llaman de esta manera

--- clase-5/datos.js
+++ clase-5/datos.js
@@ -1,14 +1,14 @@
 const ingresoDatos = document.querySelector('#Ingresar')
 
 ingresoDatos.onclick = function () {
-  let textoEdad = document.querySelector('#edad-usuario').innerText
-  let nombreDeUsuario = document.querySelector('#primer-nombre').innerText
-  let segundoNombreUsuario = document.querySelector('#segundo-nombre').innerText
-  let apellido = document.querySelector('#apellido-usuario').innerText
+  const textoEdad = document.querySelector('#edad-usuario').value
+  const nombreDeUsuario = document.querySelector('#primer-nombre').value
+  const segundoNombreUsuario = document.querySelector('#segundo-nombre').value
+  const apellido = document.querySelector('#apellido-usuario').value
 
-  let bienvenidoUsuario = 'Bienvenido' + nombreDeUsuario
-  let datosUsuario = nombreDeUsuario + segundoNombreUsuario + apellido
+  let bienvenidoUsuario = 'Bienvenido ' + nombreDeUsuario
+  let datosUsuario = nombreDeUsuario + " " + segundoNombreUsuario + " " + apellido
   document.querySelector('#bienvenido').innerText = bienvenidoUsuario
-  document.querySelector('#tabla-datos-usuario').innerText = "Nombre completo: " + datosUsuario + "Edad: " + textoEdad
+  document.querySelector('#tabla-datos-usuario').innerText = "Nombre completo: " + datosUsuario + " Edad: " + textoEdad
   return false
 };
